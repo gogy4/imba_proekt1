cc24c5a6e6d09c83f344634c8ec456cb22fe717c
Якшибаев Данил <younggogy@gmail.com>
2024-12-28 14:19:09 +0500

    сделал заглушки для всех страниц

--- pages/urls.py
+++ pages/urls.py
@@ -5,4 +5,9 @@ app_name = 'pages'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('/statistics/', views.statistics, name='statistics'),
+    path('/demand/', views.demand, name='demand'),
+    path('/geography/', views.geography, name='geography'),
+    path('/skills/', views.skills, name='skills'),
+    path('/last_vacancies/', views.last_vacancies, name='last_vacancies'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'base.html')
+
+def statistics(request):
+    return render(request, 'base.html')
+
+def demand(request):
+    return render(request, 'base.html')
+
+def geography(request):
+    return render(request, 'base.html')
+
+def skills(request):
+    return render(request, 'base.html')
+
+def last_vacancies(request):
+    return render(request, 'base.html')
