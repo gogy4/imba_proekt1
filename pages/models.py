16b3df6ea925415ade7b6be3e6f1827908a9c0a9
Якшибаев Данил <younggogy@gmail.com>
2024-12-30 15:14:06 +0500

    сделал скелет страницу geography, demand

--- pages/models.py
+++ pages/models.py
@@ -19,3 +19,33 @@ class DynamicSalaryProf(models.Model):
 class DynamicCountProf(models.Model):
     year = models.IntegerField()
     vacancy_count = models.IntegerField()
+
+class Vacancy(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    skills = models.TextField()
+    company = models.CharField(max_length=255)
+    salary_from = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    salary_to = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    salary_currency = models.CharField(max_length=4, null=True, blank=True)
+    region = models.CharField(max_length=100)
+    publication_date = models.DateTimeField()
+
+class AreaSalaryAll(models.Model):
+    area_name = models.CharField(max_length=255)
+    average_salary = models.IntegerField()
+
+
+class AreaCountAll(models.Model):
+    area_name = models.CharField(max_length=255)
+    vacancy_count = models.DecimalField(max_digits=5, decimal_places=2)
+
+
+class AreaSalaryProf(models.Model):
+    area_name = models.CharField(max_length=255)
+    average_salary = models.IntegerField()
+
+
+class AreaCountProf(models.Model):
+    area_name = models.CharField(max_length=255)
+    vacancy_count = models.DecimalField(max_digits=5, decimal_places=2)
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from .models import DynamicSalaryAll, DynamicCountAll, DynamicSalaryProf, DynamicCountProf
+from .models import DynamicSalaryAll, DynamicCountAll, DynamicSalaryProf, DynamicCountProf, AreaSalaryAll, AreaCountAll, AreaSalaryProf, AreaCountProf
+from .models import Vacancy
+import requests
+import re
 
 
 def home(request):
@@ -18,15 +21,72 @@ def demand(request):
         'dynamic_count_all': dynamic_count_all,
         'dynamic_salary_prof': dynamic_salary_prof,
         'dynamic_count_prof': dynamic_count_prof,
-        'prof': 'C++ Разработчик',
+        'prof': 'C/C++ программист',
     }
     return render(request, 'pages/demand.html', context)
 
 def geography(request):
-    return render(request, 'base.html')
+    area_salary_all = AreaSalaryAll.objects.values('area_name', 'average_salary')
+    area_count_all = AreaCountAll.objects.values('area_name', 'vacancy_count')
+    area_salary_prof = AreaSalaryProf.objects.values('area_name', 'average_salary')
+    area_count_prof = AreaCountProf.objects.values('area_name', 'vacancy_count')
+
+    context = {
+        'area_salary_all': area_salary_all,
+        'area_count_all': area_count_all,
+        'area_salary_prof': area_salary_prof,
+        'area_count_prof': area_count_prof,
+        'prof': 'C/C++ программист',
+    }
+    return render(request, 'pages/geography.html', context)
 
 def skills(request):
     return render(request, 'base.html')
 
+def clear_html(line: str) -> str:
+    return re.sub(r'<.*?>', '', line.strip())
+
+
+def get_hh_vacancies(profession):
+    url = f'https://api.hh.ru/vacancies?text={profession}&period=1&per_page=10'
+    headers = {'User-Agent': 'imbaproject/1.0 (younggogy@gmail.com)'}
+
+    response = requests.get(url, headers=headers)
+    data = response.json()
+
+    vacancies = []
+    for item in data.get('items', []):
+        vacancy_id = item.get('id', '')
+        url = f'https://api.hh.ru/vacancies/{vacancy_id}'
+        headers = {'User-Agent': 'imbaproject/1.0 (younggogy@gmail.com)'}
+        response = requests.get(url, headers=headers)
+        vacancy_data = response.json()
+
+        skills_list = vacancy_data.get('key_skills', [])
+        skills = ', '.join(skill.get('name', '') for skill in skills_list)
+
+        salary_info = vacancy_data.get('salary', {})
+        vacancy = {
+            'title': vacancy_data.get('name', ''),
+            'description': clear_html(vacancy_data.get('description', '')),
+            'skills': skills,
+            'company': vacancy_data.get('employer', {}).get('name', ''),
+            'salary_from': salary_info.get('from', None) if salary_info else None,
+            'salary_to': salary_info.get('to', None) if salary_info else None,
+            'salary_currency': salary_info.get('currency', None) if salary_info else None,
+            'region': vacancy_data.get('area', {}).get('name', ''),
+            'publication_date': item.get('published_at', '')
+        }
+        vacancies.append(vacancy)
+
+    return vacancies
+
+
 def last_vacancies(request):
-    return render(request, 'base.html')
+    profession = 'C/C++'
+    vacancies = get_hh_vacancies(profession)
+    for vacancy_data in vacancies:
+        Vacancy.objects.create(**vacancy_data)
+
+    context = {'vacancies': vacancies, 'profession': profession}
+    return render(request, 'latest_vacancies.html', context)
